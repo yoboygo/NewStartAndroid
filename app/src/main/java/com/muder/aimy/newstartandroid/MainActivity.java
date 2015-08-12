ce58e59f8b07cd32d16db5b8752c0edce271d3c6
yoboygo <yoboygo@gmail.com>
2015-08-12 16:27:47 +0800

    Add IOC

--- app/src/main/java/com/muder/aimy/newstartandroid/MainActivity.java
+++ app/src/main/java/com/muder/aimy/newstartandroid/MainActivity.java
@@ -4,13 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.muder.aimy.common.ioc.ViewInjection;
+import com.muder.aimy.common.ioc.ViewInjectionUtils;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @ViewInjection(R.id.main_bt_01)
+    private Button mainBtn01;
+    @ViewInjection(R.id.main_bt_02)
+    private Button mainBtn02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewInjectionUtils.inject(this);
+        mainBtn01.setOnClickListener(this);
+        mainBtn02.setOnClickListener(this);
+
     }
 
     @Override
@@ -33,5 +49,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.main_bt_01:
+                Toast.makeText(this,"您点击了第一个按钮",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(MainActivity.this,"你没有点击第一个按钮",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
